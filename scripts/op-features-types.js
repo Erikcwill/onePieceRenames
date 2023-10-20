1caa4eec6ad8ddb3f28fa152826bb2a646a5a7ad
Erick William <erikcwill@gmail.com>
2023-10-20 02:14:16 -0300

    update manifest

--- scripts/op-features-types.js
+++ scripts/op-features-types.js
@@ -1,23 +1,3 @@
-/*Hooks.on("init", function() {
-  game.dnd5e.config.skills["tec"] = {
-    label: "Tecnologia", // Nome da habilidade
-    ability: "wis", // Habilidade associada (Inteligência)
-  };
-});
-
-
-Hooks.on("createActor", (actor, data, options, userId) => {
-  if (actor.data.type === "character") {
-    // Certifique-se de que estamos adicionando o detalhe apenas a atores do tipo "character".
-    actor.data.data.details.dreams = {
-      label: "Sonhos",
-      value: "", // Você pode definir um valor padrão aqui, se desejar.
-    };
-    actor.update(); // Isso salvará as alterações no ator.
-  }
-});
-  */
-
 Hooks.on("init", function () {
   game.dnd5e.config.featureTypes["personality"] = {
     label: "Personality",
